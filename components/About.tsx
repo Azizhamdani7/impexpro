f6036285e4a15ef22b48b2444fcf09921ab0923b
Syed Aziz Hamdani <azizhamdani7@gmail.com>
2026-06-12 03:00:33 +0500

    test change

--- components/About.tsx
+++ components/About.tsx
@@ -4,5 +4,6 @@ import { homeHtml } from "@/lib/pageContent";
 const servicesMarker = "<!-- ═══ SERVICES ═══ -->";
 
 export function About() {
+  
   return <HtmlContent html={homeHtml.split(servicesMarker)[0]} />;
 }
